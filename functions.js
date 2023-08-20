4dfb81d190b725160a08d0340d90938c89880f57
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 21:17:06 -0300

    Created createUser() function

--- functions.js
+++ functions.js
@@ -2,6 +2,10 @@ const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
   checkValue: (value) => value,
+  createUser: () => {
+    const user = { firstName: "Name", lastName: "Lastname" };
+    return user;
+  },
 };
 
 module.exports = functions;

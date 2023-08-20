da8d39481303517a3172d85b9b61959226171d8e
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 22:52:08 -0300

    Created Tests -> check the correct solutions -> toBeTruthy / toBeFalsy matchers

--- anagrams.test.js
+++ anagrams.test.js
@@ -1,9 +1,20 @@
 const isAnagram = require("./anagrams");
 
+// typeof isAnagram is function
 test("isAnagram function exists", () => {
   expect(typeof isAnagram).toEqual("function");
 });
 
+// checks the correct solution -> toBeTruthy
 test("cinema is an anagram of iceman", () => {
   expect(isAnagram("cinema", "iceman")).toBeTruthy();
+});
+
+test("Dormitory is an anagram of dirty room##", () => {
+  expect(isAnagram("Dormitory", "dirty room##")).toBeTruthy();
+});
+
+// toBeFalsy
+test("Hello is not an anagram of Aloha", () => {
+  expect(isAnagram("Hello", "Aloha")).toBeFalsy();
 });

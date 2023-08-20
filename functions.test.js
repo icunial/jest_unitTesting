b6f04bbeda181cc8ca12a0725aa8ee516fff1d24
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 21:53:51 -0300

    Created Test -> async await

--- functions.test.js
+++ functions.test.js
@@ -53,9 +53,18 @@ test("Admin should be in usernames", () => {
 });
 
 // Working with async data
+
+// Promise
 test("User fetched name should be Leanne Graham", () => {
   expect.assertions(1);
   return functions.fetchUser().then((data) => {
     expect(data.name).toEqual("Leanne Graham");
   });
+});
+
+// Async Await
+test("User fetched name should be Leanne Graham / Async Await", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
 });

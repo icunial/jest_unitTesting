4ec8bd3843c631ebaff9a00be575e49eb12e9131
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 23:09:47 -0300

    Updated chunkArray() function -> chunk.js

--- chunk.js
+++ chunk.js
@@ -6,7 +6,6 @@ const chunkArray = (arr, len) => {
   arr.forEach((val) => {
     // Get last element
     const last = chunkedArr[chunkedArr.length - 1];
-    console.log(last);
 
     // Check if last and if last length is equal to the chunk len
     if (!last || last.length === len) {

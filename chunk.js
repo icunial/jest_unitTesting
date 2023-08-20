d9d8f69ca3288da6b42892de552486b5ef154d90
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 22:34:49 -0300

    Updated chunkArray() function -> chunk.js / Created Test -> return solution

--- chunk.js
+++ chunk.js
@@ -6,16 +6,16 @@ const chunkArray = (arr, len) => {
   arr.forEach((val) => {
     // Get last element
     const last = chunkedArr[chunkedArr.length - 1];
+    console.log(last);
 
     // Check if last and if last length is equal to the chunk len
-    if (!last || last.lenght === len) {
+    if (!last || last.length === len) {
       chunkedArr.push([val]);
     } else {
       last.push(val);
     }
-
-    return chunkedArr;
   });
+  return chunkedArr;
 };
 
 module.exports = chunkArray;

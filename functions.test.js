ac3a1d5dbe211a306652af4eb009853a151a350f
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 21:27:20 -0300

    Created Test -> toBeLessThan() / toBeGreaterThan() matchers

--- functions.test.js
+++ functions.test.js
@@ -32,3 +32,11 @@ test("User should be Name Lastname object", () => {
     lastName: "Lastname",
   });
 });
+
+// Less than and greater than
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+
+  expect(load1 + load2).toBeLessThan(1600);
+});

a549f5e04a9772233989dc9d1fa2efcb35100cb9
Ignacio Cunial <igsecu@hotmail.com>
2023-08-19 21:23:28 -0300

    Created Test -> toEqual() matcher

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,11 @@
 const functions = require("./functions");
 
+// toBe
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+// not
 test("Adds 2 + 2 to not equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
@@ -21,4 +23,12 @@ test("Should be null", () => {
 // toBeFalsy
 test("Should be falsy", () => {
   expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test("User should be Name Lastname object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Name",
+    lastName: "Lastname",
+  });
 });
